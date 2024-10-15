bae0cc9c47dc39ed23438a37d94f10ea5f48e0d9
Zeke Tinch <zeketinch@gmail.com>
2024-10-15 13:12:57 -0500

    fixed spacing issues

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,23 +1,26 @@
 export default function ContactPage() {
     return (
-        <div>
-            <div className="input-group mb-3">
-    <span className="input-group-text" id="inputGroup-sizing-sm">Name</span>
-    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
-</div>
+        <div className = "container">
+            <h1 className="col d-flex justify-content-center">Contact Me</h1>
 
-<div className="input-group mb-3">
-    <span className="input-group-text" id="inputGroup-sizing-default">Email Address</span>
-    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
-</div>
+            <section>
+                <p className="text-center">
+                    If you would like to hire me or contact me about building web pages for you please see
+                    the information below. You can find links to my GitHub, Stack overflow and my Linkedin on 
+                    the footer of the page.
+                </p>
+            </section>
 
-<div className="input-group mb-3">
-    <span className="input-group-text" id="inputGroup-sizing-lg">Message</span>
-    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
-</div>
-<div className="col-12">
-    <button className="btn btn-primary" type="submit">Submit form</button>
-</div>
+            <ul>
+                <h1 className="col d-flex justify-content-center">Google Email</h1>
+                <li className="col d-flex justify-content-center">ZekeTinch@gmail.com</li>
+            </ul>
+
+            <ul>
+                <h1 className="col d-flex justify-content-center">Cellphone Number</h1>
+                <li className="col d-flex justify-content-center">931-200-2927</li>
+            </ul>
+
         </div>
     )
 }
